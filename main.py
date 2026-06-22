40b8fa58a07e01ab7970e457337ace4f84bbb8cf
Aditya <adityakrr9973@gmail.com>
2026-06-22 08:52:34 +0530

    fixes & tracing

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ load_dotenv()
 import os 
 os.environ['GOOGLE_API_KEY']=os.getenv('GOOGLE_API_KEY')
 os.environ['LANGSMITH_TRACING']='true'
-os.environ['LANGSMITH_PROJECT']=os.getenv['LANGSMITH_PROJECT']
-os.environ['LANGCHAIN_API_KEY']=os.getenv['LANGCHAIN_API_KEY']
-os.environ['OPENAI_API_KEY']=os.getenv['OPENAI_API_KEY']
-os.environ['LANGSMITH_ENDPOINT']=os.getenv['LANGSMITH_ENDPOINT']
+os.environ['LANGSMITH_PROJECT']=os.getenv('LANGSMITH_PROJECT')
+os.environ['LANGCHAIN_API_KEY']=os.getenv('LANGCHAIN_API_KEY')
+os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
+os.environ['LANGSMITH_ENDPOINT']=os.getenv('LANGSMITH_ENDPOINT')
 
 
 
